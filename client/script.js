b4717b827b5b2d639c3750a44de948b0b048939b
Krystian <kris.akuzino@gmail.com>
2022-12-26 17:48:22 +0100

    update the script

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
 
-    const response = await fetch('https://code-by-kris.onrender.com/', {
+    const response = await fetch('https://test2-xehj.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        const parsedData = data.bot.trim() 
 
         typeText(messageDiv, parsedData)
     } else {
